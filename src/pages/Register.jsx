641509a8e7830fd5168d43827a14605a5bad89d4
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-05-14 16:42:21 -0700

    Added token handling and password confirmation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
+export async function registerAction({ request }) {
+    try {
+        const loginData = await request.formData();
+        const username = loginData.get("username");
+        const password = loginData.get("password");
+        const passwordConfirm = loginData.get("password-confirm");
+        if (password !== passwordConfirm) {
+            throw new Error(
+                "The values in the password and confirm password fields must match"
+            );
+        }
+        const res = await fetch("http://127.0.0.1:3000/api/v1/users", {
+            method: "POST",
+            body: JSON.stringify({ username, password, passwordConfirm }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!res.ok) {
+            throw new Error(`Status error ${res.status}`);
+        }
+        const data = await res.json();
+        sessionStorage.setItem("role", data.role);
+        sessionStorage.setItem("username", data.username);
+        sessionStorage.setItem("_id", data._id);
+        sessionStorage.setItem("token", data.token);
+        return data;
+    } catch (error) {
+        const errorMessage = { status: error.message };
+        return errorMessage;
+    }
+}
+
 export default function Register() {
     return (
         <Form className="login-form" ref={loginForm} method="post">
@@ -35,7 +68,7 @@ export default function Register() {
                 id="register-password-confirm"
                 className="input"
                 type="text"
-                name="confirm-password"
+                name="password-confirm"
                 pattern="[a-z0-9]{4,18}"
                 title="Letters and numbers only, between 4 and 18 characters"
                 required
