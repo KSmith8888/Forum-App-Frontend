93cb1ea6503ab0f707e540f7b581267592fc4929
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-06-13 17:36:23 -0700

    Updated footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p className="footer-text">
+            <p>
                 This app stores information in the browser for necessary
                 functionality and to improve user experience. For more
                 information, see our <Link to="/terms">Terms of Service</Link>
             </p>
-            <p className="footer-text">
+            <p>
                 This service bears no responsibility for the content of third
                 party sites linked to from within the service
             </p>
