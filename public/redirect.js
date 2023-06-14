db6cf80ee5c5a36dbb3d9f86f494d98f7d11cd89
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-06-14 14:59:43 -0700

    Split redirect logic into external script

---
+++ public/redirect.js
@@ -0,0 +1,13 @@
+let params = null;
+const route = location.pathname ? `route=${location.pathname}` : null;
+const id = sessionStorage.getItem("_id");
+const isUserLoggedIn = id ? "status=logged in" : null;
+if (route && isUserLoggedIn) {
+    params = `?${route}&${isUserLoggedIn}`;
+} else if (route && !isUserLoggedIn) {
+    params = `?${route}`;
+} else if (!route && isUserLoggedIn) {
+    params = `?${isUserLoggedIn}`;
+}
+const redirectUrl = params ? `/${params}` : "/";
+location.assign(redirectUrl);
