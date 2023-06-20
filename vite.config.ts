405df4329e66aa9c2e2aba402e3f80c65cda6b04
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-06-20 16:03:36 -0700

    Updated target to es2021

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     base: "./",
     plugins: [react(), splitVendorChunkPlugin()],
+    build: {
+        target: "es2021",
+    },
 });
