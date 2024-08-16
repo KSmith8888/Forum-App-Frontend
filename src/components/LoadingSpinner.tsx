0c03fe2707aea5907f5b68092cc04ed986fc3956
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2024-08-15 17:57:18 -0700

    Created LoadingSpinner and added purpose to manifest images

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import "../assets/styles/loading.css";
+
+export default function LoadingSpinner() {
+    return (
+        <div id="loading-area">
+            <div id="loading-spinner"></div>
+            <p id="loading-text">Loading</p>
+        </div>
+    );
+}
