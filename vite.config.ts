a5f2e47260c8b8e209869524b2484d1abeaf37f2
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2024-08-19 18:10:05 -0700

    Updated target to 2022

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [react()],
     build: {
-        target: ["es2021", "edge88", "firefox98", "chrome87", "safari15.4"],
+        target: "es2022",
         assetsInlineLimit: 0,
-        rollupOptions: {
-            output: {
-                manualChunks: (id: string) => {
-                    if (id.includes("node_modules")) {
-                        if (id.includes("router")) {
-                            return "vendor_router";
-                        } else {
-                            return "vendor_react";
-                        }
-                    }
-                },
-            },
-        },
     },
 });
