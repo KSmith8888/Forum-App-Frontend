4617a7228e844c1f6e4dcf1992377dbb7a926adc
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2024-12-19 17:42:35 -0800

    Started updating imports for react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 import MainLayout from "./MainLayout.tsx";
 import ErrorElement from "./Error.tsx";
